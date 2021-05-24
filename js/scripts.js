2137cb7bcaefd35bba648f0398f256106405ea5a
Johannes van Woerkom <johannesvw3@gmail.com>
2021-05-24 15:35:52 -0400

    Created Pokemon List Array (Database)

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-alert('hello world');
+let pokemonList = [
+    {name: "Bulbasaur", height: 7, types: ["grass", "Poison"]},
+    {name: "Charmander", height: 6, types: ["Fire", "Normal"]},
+    {name: "Squritle", height: 5, types: ["Water", "Ice"]}];
 
-let favoriteFood = 'pizza'
-document.write('My favorite food is ' + favoriteFood);
