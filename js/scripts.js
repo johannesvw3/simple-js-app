65fe4ac76a8e934dca8548561b515c3f07426f15
Johannes van Woerkom <johannesvw3@gmail.com>
2021-05-25 11:42:56 -0400

    Added Loops and Conditionals Statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
 let pokemonList = [
-    {name: "Bulbasaur", height: 7, types: ["grass", "Poison"]},
-    {name: "Charmander", height: 6, types: ["Fire", "Normal"]},
-    {name: "Squritle", height: 5, types: ["Water", "Ice"]}];
+    {name: "Bulbasaur", height: 0.7, types: ["grass", "Poison"], evolutions: ["Ivysaur", "Venusaur"]},
+    {name: "Charmander", height: 0.6, types: ["Fire", "Normal"], evolutions: ["Charmeleon", "Charizard"]},
+    {name: "Squritle", height: 0.5, types: ["Water", "Ice"], evolutions: ["Wartortle", "Blastoise"]},
+    {name: "Eevee", height: 0.3, types: ["Normal"], evolutions: ["Umbreon", "Sylveon", "Leafeon", "Glaceon", "Vaporeon", "Jolteon", "Flareon", "Espeon"]}
+    ];
+
+    for (let i = 0; i < pokemonList.length; i++) {
+        if (pokemonList[i].height <= 0.6) {
+            document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + ")" + " ")
+            }
+
+        if (pokemonList[i].height > 0.6) {
+            document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + ")" + " Wow, that's big! ")
+            }
+        }; 
+
 
