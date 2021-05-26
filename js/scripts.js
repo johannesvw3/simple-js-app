ce656f4eedf3c1d419a553856b42b59858ded28b
Johannes van Woerkom <johannesvw3@gmail.com>
2021-05-26 11:43:00 -0400

    pokemonList inside IIFE & add function

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+pokemonRepository = (function () {
+
+
 let pokemonList = [
     {name: "Bulbasaur", height: 0.7, types: ["grass", "Poison"], evolutions: ["Ivysaur", "Venusaur"]},
     {name: "Charmander", height: 0.6, types: ["Fire", "Normal"], evolutions: ["Charmeleon", "Charizard"]},
@@ -5,16 +8,34 @@ let pokemonList = [
     {name: "Eevee", height: 0.3, types: ["Normal"], evolutions: ["Umbreon", "Sylveon", "Leafeon", "Glaceon", "Vaporeon", "Jolteon", "Flareon", "Espeon"]}
     ];
 
-    pokemonList.forEach(function(pokemon) {
-        if (pokemon.height <= 0.6) {
-            document.write(pokemon.name + " (Height: " + pokemon.height + ")" + '<br>')
-            }
-    });
+    
 
-        pokemonList.forEach(function(pokemon) {
-        if (pokemon.height > 0.6) {
-            document.write(pokemon.name + " (Height: " + pokemon.height + ")" + " Wow, That's Big!")
-            }
-    });
+    function getAll() {
+        return pokemonList;
+    }
+
+    function add(pokemon) {
+        pokemonList.push(pokemon)
+    }
+
+
+    return {
+    add: add,
+    getAll: getAll
+    };
+
+})();
+
+    pokemonRepository.getAll().forEach(function(pokemon) {
+    if (pokemon.height <= 0.6) {
+        document.write(pokemon.name + " (Height: " + pokemon.height + ")" + '<br>')
+        }
+});
+
+    pokemonRepository.getAll().forEach(function(pokemon) {
+    if (pokemon.height > 0.6) {
+        document.write(pokemon.name + " (Height: " + pokemon.height + ")" + " Wow, That's Big!")
+        }
+});
 
 
