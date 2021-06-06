668befb62e7891c7057953774ba8b2d8eeb9ce23
Johannes van Woerkom <johannesvw3@gmail.com>
2021-06-06 12:58:53 -0400

    CSS style added / Added Buttons for Pokemons in DOM
    Changed background color and styled buttons.
    
    Made pokemons into buttons and added event listener

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,55 @@
 pokemonRepository = (function () {
 
+let pokedexPokemonList = document.querySelector('.pokemon-list')
 
 let pokemonList = [
     {name: "Bulbasaur", height: 0.7, types: ["grass", "Poison"], evolutions: ["Ivysaur", "Venusaur"]},
     {name: "Charmander", height: 0.6, types: ["Fire", "Normal"], evolutions: ["Charmeleon", "Charizard"]},
-    {name: "Squritle", height: 0.5, types: ["Water", "Ice"], evolutions: ["Wartortle", "Blastoise"]},
+    {name: "Squirtle", height: 0.5, types: ["Water", "Ice"], evolutions: ["Wartortle", "Blastoise"]},
     {name: "Eevee", height: 0.3, types: ["Normal"], evolutions: ["Umbreon", "Sylveon", "Leafeon", "Glaceon", "Vaporeon", "Jolteon", "Flareon", "Espeon"]}
     ];
-
-    
 
     function getAll() {
         return pokemonList;
     }
 
     function add(pokemon) {
-        pokemonList.push(pokemon)
+        typeof pokemon == 'object' ? pokemonList.push(pokemon) : console.log('wrong format')
     }
 
+    function addListItem(pokemon){
+        let pokemonItem = document.createElement('li');
+        let pokemonButton = document.createElement('button');
+            pokemonButton.innerHTML = pokemon.name;
+        pokemonButton.classList.add('pokemon-button');
+        pokedexPokemonList.appendChild(pokemonItem);
+        pokemonItem.appendChild(pokemonButton);
+        pokemonButton.addEventListener('click', function () {
+            showDetails(pokemon);
+        });
+    }
+
+    function showDetails(pokemon) {
+        console.log(pokemon)
+    }
 
     return {
     add: add,
-    getAll: getAll
+    getAll: getAll,
+    addListItem: addListItem,
+    showDetails: showDetails
     };
 
 })();
 
-    pokemonRepository.getAll().forEach(function(pokemon) {
-    if (pokemon.height <= 0.6) {
-        document.write(pokemon.name + " (Height: " + pokemon.height + ")" + '<br>')
-        }
+pokemonRepository.add({ name: "Pikachu", height: 0.4 });
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.getAll().forEach(function(pokemon) {
+    pokemonRepository.addListItem(pokemon)
 });
 
-    pokemonRepository.getAll().forEach(function(pokemon) {
-    if (pokemon.height > 0.6) {
-        document.write(pokemon.name + " (Height: " + pokemon.height + ")" + " Wow, That's Big!")
-        }
-});
-
-
+/*
+pokemon.height > 0.6 ?  document.write(pokemon.name + " (Height: " + pokemon.height + ")" + " Wow, That's Big!" + '<br>') 
+    : document.write(pokemon.name + " (Height: " + pokemon.height + ")" + '<br>');
+    */
