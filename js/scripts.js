3172f2abad86d53eccd34cdd63fdc74a1c45d0c9
Johannes van Woerkom <johannesvw3@gmail.com>
2021-06-18 13:43:38 -0400

    CSS: Centered Content - JS: Added Modal for details display

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,21 @@ pokemonRepository = (function () {
 
 let pokedexPokemonList = document.querySelector('.pokemon-list');
 let pokedexScreen = document.querySelector('.pokedex-screen');
+let modalContainer = document.querySelector('#modal-container');
+let modal = document.querySelector('.modal');
+
+let modalClose = document.createElement('button');
+  modalClose.classList.add('modal-close');
+let pokeName = document.createElement('h1');
+ pokeName.classList.add('Pokename');
+let pokeHeight = document.createElement('p');
+ pokeHeight.classList.add('Pokeheight');
+let imageContainer = document.createElement('div');
+  imageContainer.classList.add('img-container');
+let pokeImage = document.createElement('img');
+  pokeImage.classList.add('PokeImage');
+let pokeType = document.createElement('p');
+  pokeType.classList.add('Poketype');
 
 let pokemonList = [];
 let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
@@ -17,7 +32,7 @@ let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
     function addListItem(pokemon){
         let pokemonItem = document.createElement('li');
         let pokemonButton = document.createElement('button');
-            pokemonButton.innerHTML = pokemon.name;
+            pokemonButton.innerHTML = pokemon.name.toUpperCase();
         pokemonButton.classList.add('pokemon-button');
         pokedexPokemonList.appendChild(pokemonItem);
         pokemonItem.appendChild(pokemonButton);
@@ -26,12 +41,47 @@ let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
         });
     }
 
+    function showModal() {
+      modalContainer.classList.add('is-visible');
+    }
+
+//Hide Modal Event Listeners
+
+    function hideModal() {
+      modalContainer.classList.remove('is-visible');
+    }
+
+    modalClose.addEventListener('click' , hideModal);
+
+    window.addEventListener('keydown', (e) => {
+      let modalContainer = document.querySelector('#modal-container');
+      if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+        hideModal();  
+      }
+    });
+   
+    modalContainer.addEventListener('click', (e) => {
+      let target = e.target;
+      if (target === modalContainer) {
+        hideModal();
+      }
+    });
+
+//Pokemon Details Modal
+
     function showDetails(pokemon) {
         loadDetails(pokemon).then(function () {
-        console.log(pokemon)
+        pokeName.innerHTML = pokemon.name.toUpperCase();
+        pokeHeight.innerHTML = 'Height: ' + pokemon.height;
+        pokeType.innerHTML = 'Type: ' + pokemon.types.toUpperCase();
+        pokeImage.src = pokemon.imageUrl;
+        modalClose.innerHTML = "Close";
+        showModal();
         });
     }
 
+//Get list from pokeapi API
+  
     function loadList() {
         return fetch(apiUrl).then(function (response) {
           return response.json();
@@ -55,11 +105,20 @@ let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
           }).then(function (details) {
             item.imageUrl = details.sprites.front_default;
             item.height = details.height;
-            item.types = details.types;
+            item.types = details.types[0].type.name;
           }).catch(function (e) {
               console.error(e);
           });
      }
+
+     modal.appendChild(modalClose);
+     modal.appendChild(pokeName);
+     modal.appendChild(pokeHeight);
+     modal.appendChild(pokeType);
+     modal.appendChild(imageContainer);
+     imageContainer.appendChild(pokeImage);
+     
+//Functions
 
     return {
     add: add,
@@ -67,7 +126,9 @@ let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
     addListItem: addListItem,
     showDetails: showDetails,
     loadList: loadList,
-    loadDeatails: loadDetails
+    loadDeatails: loadDetails,
+    showModal: showModal,
+    hideModal: hideModal
     };
 
 })();
